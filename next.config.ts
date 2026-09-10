0d017f1697170617163ed45799ea53426a40ccd5
sahrulbuamona <sahrulbuamona@gmail.com>
2026-09-10 22:33:30 +0900

    ix Vercel ENOENT

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  // output: "standalone", // Buka komentar ini HANYA jika Anda mem-build menggunakan Docker
 };
 
 export default nextConfig;
